7716b6e9bf6296fbe2587da183a90df660beab10
David Antognoli <david.antognoli@gmail.com>
2021-04-14 20:38:20 -0500

    updated animation system to use state machine behaviour

--- Assets/Scripts/Third Person Starter Kit/Npc.cs
+++ Assets/Scripts/Third Person Starter Kit/Npc.cs
@@ -7,7 +7,7 @@ public class Npc : MonoBehaviour
 {
     [Tooltip("Npc waits this long before 'rolling' a random chance to change idle animations.")]
     [SerializeField]
-    private float minTimeBetweenIdleAnimationChangeRolls = 2, maxTimeBetweenIdleAnimationChangeRolls = 5;
+    private float minTimeBetweenIdleAnimationChangeRolls = 3, maxTimeBetweenIdleAnimationChangeRolls = 6;
 
     [Tooltip("We switch the parent gameobject's animator component to use this when the game starts.")]
     [SerializeField]
@@ -15,21 +15,18 @@ public class Npc : MonoBehaviour
 
     private Animator animator;
     private int cycleOffsetValueAnimParam = Animator.StringToHash("CycleOffsetValue");
-    private int randomAnimationValueAnimParam = Animator.StringToHash("RandomAnimationValue");
     private float cycleOffsetValue;
-    private bool shouldUpdateIdleAnimations = true;
-    private int maxRoll, minRoll;
     private float timeBetweenIdleAnimationChangeRolls;
+
+    public float MinTimeBetweenIdleAnimationChangeRolls => minTimeBetweenIdleAnimationChangeRolls;
+    public float MaxTimeBetweenIdleAnimationChangeRolls => maxTimeBetweenIdleAnimationChangeRolls;
 
     // Start is called before the first frame update
     void Start()
-    {
-        timeBetweenIdleAnimationChangeRolls = 
-            UnityEngine.Random.Range(minTimeBetweenIdleAnimationChangeRolls, maxTimeBetweenIdleAnimationChangeRolls);
+    {      
         animator = GetComponentInParent<Animator>();
         animator.runtimeAnimatorController = animatorController;
         animator.SetFloat(cycleOffsetValueAnimParam, UnityEngine.Random.value);
-        StartCoroutine(ChangeIdleAnimationCoroutine());
     }
 
     /// <summary>
@@ -37,25 +34,7 @@ public class Npc : MonoBehaviour
     /// The goal is to have NPCs not all play the same animations and not feel too uniform or static.
     /// </summary>
     /// <returns></returns>
-   private IEnumerator ChangeIdleAnimationCoroutine()
-    {
-        while (shouldUpdateIdleAnimations)
-        {
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
-            {
-                yield return new WaitForSeconds(timeBetweenIdleAnimationChangeRolls);
-                animator.SetInteger(randomAnimationValueAnimParam, MakeAnimationChangeRoll());
-            }
-            else
-            {
-                animator.SetInteger(randomAnimationValueAnimParam, 0);
-                yield return null;
-            }        
-        }
-    }
 
-    private int MakeAnimationChangeRoll()
-    {
-        return UnityEngine.Random.Range(1, 11);
-    }
+
+
 }
