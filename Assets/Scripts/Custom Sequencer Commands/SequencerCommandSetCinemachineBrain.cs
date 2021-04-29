1ca0f7cafb1a5611a42de5d99d4ca0392015dbce
David Antognoli <david.antognoli@gmail.com>
2021-04-29 13:34:38 -0500

    add SetCinemachineBraine Sequencer Command

---
+++ Assets/Scripts/Custom Sequencer Commands/SequencerCommandSetCinemachineBrain.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+using Cinemachine;
+
+namespace PixelCrushers.DialogueSystem.SequencerCommands
+{
+    [AddComponentMenu("")] // Hide from menu.
+    public class SequencerCommandSetCinemachineBrain : SequencerCommand
+    {
+        private CinemachineBrain cinemachineBrain;
+
+        public void Awake()
+        {
+            cinemachineBrain = Camera.main.GetComponent<CinemachineBrain>();
+            cinemachineBrain.enabled = GetParameterAsBool(0);
+            Stop();
+        }
+    }
+}
